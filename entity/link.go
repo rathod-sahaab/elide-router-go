3b347a7ae9284a320e2a99ea39b7819c58acc263
Abhay Raj Singh <abhay@tealfeed.com>
2022-06-17 02:43:08 +0530

    Added link creation

--- entity/link.go
+++ entity/link.go
@@ -12,3 +12,35 @@ type Link struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+
+func NewLink(slug string, target string, creatorID ID, groupID ID, projectID ID) (*Link, error) {
+	link := Link{
+		ID:        NewID(),
+		Slug:      slug,
+		Target:    target,
+		CreatorID: creatorID,
+		GroupID:   groupID,
+		ProjectID: projectID,
+		CreatedAt: time.Now(),
+	}
+
+	err := link.Validate()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &link, nil
+}
+
+func (link *Link) Validate() error {
+	if link.Slug == "" {
+		return ErrInvalidEntity
+	}
+
+	if link.Target == "" {
+		return ErrInvalidEntity
+	}
+
+	return nil
+}
